18f6dceb8bc466ff227a67d1a1dd73dcac766cb6
RomanDeveloperAcc <roma04dan@gmail.com>
2020-07-11 02:12:50 +0300

    added header markup and stylization

--- src/app/components/general/header/header.component.ts
+++ src/app/components/general/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  public capImg = 'assets/img/core/chief-cap.png';
   constructor() { }
 
   ngOnInit(): void {
