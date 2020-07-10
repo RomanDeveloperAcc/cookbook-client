04da9c375970cc0b39fbc3444b834d8dbb69ce03
RomanDeveloperAcc <roma04dan@gmail.com>
2020-07-11 02:16:25 +0300

    hierarchy and minor fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './components/general/home/home.component';
+import { HomeComponent } from './components/homepage/home/home.component';
 
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/general/home/home.component';
+import { HomeComponent } from './components/homepage/home/home.component';
 import { RecipesListComponent } from './components/recipes/recipes-list/recipes-list.component';
 import { RecipesItemComponent } from './components/recipes/recipes-item/recipes-item.component';
-import { HeaderComponent } from './components/general/header/header.component';
-import { FooterComponent } from './components/general/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
