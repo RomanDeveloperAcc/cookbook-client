f2b250c8ad1ae32ca0f7c127cd349b7becd42a9f
RomanDeveloperAcc <roma04dan@gmail.com>
2020-07-11 01:32:07 +0300

    added footer and header startin component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/general/home/home.component';
 import { RecipesListComponent } from './components/recipes/recipes-list/recipes-list.component';
 import { RecipesItemComponent } from './components/recipes/recipes-item/recipes-item.component';
+import { HeaderComponent } from './components/general/header/header.component';
+import { FooterComponent } from './components/general/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     RecipesListComponent,
-    RecipesItemComponent
+    RecipesItemComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
