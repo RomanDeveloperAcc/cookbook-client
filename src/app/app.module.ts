cf3d3ed6b52a83587e259a6b855e5fbf8390602e
RomanDeveloperAcc <roma04dan@gmail.com>
2020-07-11 01:27:00 +0300

    added starting components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/general/home/home.component';
+import { RecipesListComponent } from './components/recipes/recipes-list/recipes-list.component';
+import { RecipesItemComponent } from './components/recipes/recipes-item/recipes-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RecipesListComponent,
+    RecipesItemComponent
   ],
   imports: [
     BrowserModule,
